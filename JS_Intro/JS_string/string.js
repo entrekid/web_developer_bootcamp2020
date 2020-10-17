d9d7264ab74b899798152c3da8cd6a936173ced2
entrekid <dat.sci.seol@gmail.com>
2020-10-17 20:37:43 +0900

    string learnt

---
+++ JS_Intro/JS_string/string.js
@@ -0,0 +1,39 @@
+let firstName = "ziggy";
+let msg = "Please do not feed the chimps!";
+let animal = 'Dumbo Octopus';
+
+let yellMsg = msg.toUpperCase();
+console.log(yellMsg)
+
+let greeting = "    leave me alone plz    ";
+console.log(greeting);
+
+greeting = greeting.trim();
+console.log(greeting);
+
+let tvShow = "catdog";
+console.log(tvShow.indexOf("cat"));
+console.log(tvShow.indexOf("hi"));
+console.log(tvShow.indexOf("cav"));
+console.log(tvShow.indexOf("dog"));
+
+let str = "supercalifragilisticexpialidocious";
+
+console.log(str.slice(0, 5));
+
+console.log(str.slice(5));
+
+console.log("5" + "4");
+
+console.log("pup"[3])
+
+let park = "Yellowstone";
+const index = park.indexOf("Stone");
+console.log(index);
+
+let yell = "GO AWAY!!";
+let index2 = yell.indexOf("!");
+console.log(index2);
+
+let str2 = 'thing "thing"'
+console.log(str2)
